92a084fa6e940158a9d1f2555ab9d4dec0c4d7df
DreadKyller <dreadkyller@gmail.com>
2011-06-11 21:48:21 -0400

    Edited and Fixed over 50 Bugs, Added more features

--- src/org/styllex/clipboard/Mega.java
+++ src/org/styllex/clipboard/Mega.java
@@ -5,22 +5,25 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Set;
 
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Block;
-import org.bukkit.command.*;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.util.config.*;
+import org.bukkit.util.config.Configuration;
 import org.styllex.clipboard.listener.IC3D_Listener_10_90_13;
 
 public class Mega extends JavaPlugin{
 	
 	private Configuration conf;
 	private HashMap<Block, Player> clip = new HashMap<Block, Player>();
+	
 	private File file = new File("plugins/Clipboard/config.yml");
 	
 	private IC3D_Listener_10_90_13 pl = new IC3D_Listener_10_90_13(this);
@@ -28,15 +31,24 @@ public class Mega extends JavaPlugin{
 	private boolean enabled=true;
 	private int tool_id= -1;
 	private String tool_name="stick";
-	private boolean per_person=true;
+	private String cube_tool_name="string";
+	private int cube_tool_id=-1;
+	private HashMap<Player, Location> c1 = new HashMap<Player, Location>();
+	private HashMap<Player, Location> c2 = new HashMap<Player, Location>();
+	public HashMap<Player, Integer> c3 = new HashMap<Player, Integer>();
+	
+	private String lcommand;
 	
 	@Override
 	public void onDisable(){
+		conf.save();
 		System.out.println("Clipboard has been disabled");
 		getServer().broadcastMessage("Clipboard has been disabled");
 	}
 	@Override
 	public void onEnable(){
+		
+		lcommand="clipboard";
 		
 		//<!-- CONFIGURATION HANDELING -->
 		
@@ -61,17 +73,21 @@ public class Mega extends JavaPlugin{
 				
 				/*-init-*/conf.load();
 				/*-init-*/conf.setProperty("set-enabled", "true");
-				/*-init-*/conf.setProperty("selection-item-id", "~");
+				/*-init-*/conf.setProperty("selection-item-id", "-1");
 				/*-init-*/conf.setProperty("selection-item-name", "stick");
-				/*-init-*/conf.setProperty("clip-per-person", "true");
+				/*-init-*/conf.setProperty("cube-selection-item-name", "string");
+				/*-init-*/conf.setProperty("cube-selection-item-id", "-1");
 				/*-init-*/conf.save();
 				
 				System.out.println("Config file initalized");
 			}else{
+				conf.load();
 				enabled=toBool((String)conf.getProperty("set-enabled"));
 				tool_id=Integer.parseInt((String)conf.getProperty("selection-item-id"));
 				tool_name=(String)conf.getProperty("selection-item-name");
-				per_person=toBool((String)conf.getProperty("clip-per-person"));
+				cube_tool_name=(String)conf.getProperty("cube-selection-item-name");
+				cube_tool_id=Integer.parseInt((String)conf.getProperty("cube-selection-item-id"));
+				conf.save();
 			}
 			if(!enabled){
 				getServer().broadcastMessage("cb disabled...");
@@ -93,11 +109,26 @@ public class Mega extends JavaPlugin{
 		Set<Block> hset=hash.keySet();
 		Object[] obarray = hset.toArray();
 		Block[] barray=new Block[hset.size()];
+		int i=0;
 		for(Object ob : obarray){
-			barray[barray.length]=(Block)ob;
+			barray[i]=(Block)ob;
+			i++;
 		}
 		return barray;
 	}
+	public Object[][] HashToArray(HashMap<Block, Material> hash){
+		Set<Block> hset=hash.keySet();
+		Object[] obarray = hset.toArray();
+		Object[][] barray=new Block[hset.size()][2];
+		int i=0;
+		for(Object ob : obarray){
+			barray[i][0]=(Block)ob;
+			barray[i][1]=hash.get((Block)ob);
+			i++;
+		}
+		return barray;
+	}
+	
 	public boolean toBool(String s){
 		if(s.equalsIgnoreCase("true")){
 			return true;
@@ -112,9 +143,11 @@ public class Mega extends JavaPlugin{
 					sender.sendMessage("/"+cmd.getName()+" {wall} {height} // will copy the shape upward the specified layers to make a wall");
 					sender.sendMessage("/"+cmd.getName()+" {shift} {x_amount} {y_amount} {z_amount} // move the selected over some units");
 					sender.sendMessage("/"+cmd.getName()+" {pan} {x_amount} {z_amount} // move the selected along a plane");
-					sender.sendMessage("/"+cmd.getName()+" {paste} // this will past from where you are standing as first block");
+					sender.sendMessage("/"+cmd.getName()+" {delete/remove} // delete all selected blocks");
+					sender.sendMessage("/"+cmd.getName()+" {type} (MaterialName) // change the block type of all selected to the input material");
 					sender.sendMessage("/"+cmd.getName()+" {clear} // clear your clipboard");
 				}
+				registerLastCommand(cmd.getName(), args);
 				return true;
 			}
 			if(args[0].equalsIgnoreCase("wall")){
@@ -124,6 +157,7 @@ public class Mega extends JavaPlugin{
 					sender.sendMessage("up the number on layers you input.");
 					sender.sendMessage("This is for making walls and other");
 					sender.sendMessage("high structures");
+					registerLastCommand(cmd.getName(), args);
 					return true;
 				}
 				int height = Integer.parseInt(args[1]);
@@ -139,13 +173,39 @@ public class Mega extends JavaPlugin{
 						}
 					}
 				}
+				registerLastCommand(cmd.getName(), args);
 				return true;
+			}
+			if(args[0].equalsIgnoreCase("cube")||args[0].equalsIgnoreCase("box")||args[0].equalsIgnoreCase("cuboid")){
+				int blocks = 0;
+				for(double x = Math.min(this.c1.get((Player)sender).getX(), this.c2.get((Player)sender).getX());x<Math.max(this.c1.get((Player)sender).getX(), this.c2.get((Player)sender).getX())+1;x++){
+					for(double y = Math.min(this.c1.get((Player)sender).getY(), this.c2.get((Player)sender).getY());y<Math.max(this.c1.get((Player)sender).getY(), this.c2.get((Player)sender).getY())+1;y++){
+						for(double z = Math.min(this.c1.get((Player)sender).getZ(), this.c2.get((Player)sender).getZ());z<Math.max(this.c1.get((Player)sender).getZ(), this.c2.get((Player)sender).getZ())+1;z++){
+							Block block = ((Player)sender).getWorld().getBlockAt(new Location(((Player)sender).getWorld(), x, y, z));
+							if(block.getType()!=Material.AIR){
+								addClip(block, (Player)sender, false);
+								blocks++;
+							}
+						}
+					}
+				}
+				sender.sendMessage(ChatColor.GOLD+"You just selected "+blocks+" blocks!");
+			}
+			if(args[0].equalsIgnoreCase("uncube")||args[0].equalsIgnoreCase("unbox")||args[0].equalsIgnoreCase("uncuboid")||args[0].equalsIgnoreCase("un_cube")||args[0].equalsIgnoreCase("un_box")||args[0].equalsIgnoreCase("un_cuboid")){
+				if(this.c1.containsKey((Player)sender)){
+					this.c1.remove((Player)sender);
+				}
+				if(this.c2.containsKey((Player)sender)){
+					this.c2.remove((Player)sender);
+					sender.sendMessage("you have cleared your Corner Selection");
+				}
 			}
 			if(args[0].equalsIgnoreCase("shift")){
 				if(args.length<4){
 					sender.sendMessage("/"+cmd.getName()+" {shift} {x_amount} {y_amount} {z_amount}");
 					sender.sendMessage("This will move everything selected over the selected units");
 					sender.sendMessage("and will fill the original places with air");
+					registerLastCommand(cmd.getName(), args);
 					return true;
 				}
 				int xa = Integer.parseInt(args[1]);
@@ -153,6 +213,7 @@ public class Mega extends JavaPlugin{
 				int za = Integer.parseInt(args[3]);
 				Block[] bl = hashToArray(clip);
 				World world = ((Player)sender).getWorld();
+				HashMap<Block, Material> blocks = new HashMap<Block, Material>();
 				for(Block b : bl){
 					if(clip.get(b).getDisplayName().equalsIgnoreCase(((Player)sender).getDisplayName())){
 						Location loc = b.getLocation();
@@ -160,38 +221,57 @@ public class Mega extends JavaPlugin{
 						loc.setY(loc.getY()+ya);
 						loc.setZ(loc.getZ()+za);
 						Block block=world.getBlockAt(loc);
-						block.setType(b.getType());
+						blocks.put(block, b.getType());
 						b.setType(Material.AIR);
-						Player player = clip.get(b);
-						clip.remove(b);
-						clip.put(block, player);
 					}
 				}
+				
+				Set<Block> bll = blocks.keySet();
+				
+				for(Block block : bll){
+					
+					Block b = block;
+					
+					b.setType(blocks.get(b));
+					
+				}
+				
+				registerLastCommand(cmd.getName(), args);
 				return true;
 			}
 			if(args[0].equalsIgnoreCase("pan")){
 				if(args.length<3){
 					sender.sendMessage("/"+cmd.getName()+" {pan} {x_amount} {z_amount}");
 					sender.sendMessage("Same as shift only with locked height");
+					registerLastCommand(cmd.getName(), args);
 					return true;
 				}
 				int xa = Integer.parseInt(args[1]);
 				int za = Integer.parseInt(args[2]);
 				Block[] bl = hashToArray(clip);
 				World world = ((Player)sender).getWorld();
+				HashMap<Block, Material> blocks = new HashMap<Block, Material>();
 				for(Block b : bl){
 					if(clip.get(b).getDisplayName().equalsIgnoreCase(((Player)sender).getDisplayName())){
 						Location loc = b.getLocation();
 						loc.setX(loc.getX()+xa);
 						loc.setZ(loc.getZ()+za);
 						Block block=world.getBlockAt(loc);
-						block.setType(b.getType());
+						blocks.put(block, b.getType());
 						b.setType(Material.AIR);
-						Player player = clip.get(b);
-						clip.remove(b);
-						clip.put(block, player);
 					}
 				}
+				Set<Block> bll = blocks.keySet();
+				
+				for(Block block : bll){
+					
+					Block b = block;
+					
+					b.setType(blocks.get(b));
+					
+				}
+				
+				registerLastCommand(cmd.getName(), args);
 				return true;
 			}
 			if(args[0].equalsIgnoreCase("clear")){
@@ -201,16 +281,84 @@ public class Mega extends JavaPlugin{
 						clip.remove(b);
 					}
 				}
+				registerLastCommand(cmd.getName(), args);
 				return true;
+			}
+			if(args[0].equalsIgnoreCase("l")||args[0].equalsIgnoreCase("last")){
+				if(args.length==1){
+					getServer().dispatchCommand(sender, this.lcommand);
+					return true;
+				}
+				int amount = Integer.parseInt(args[0]);
+				for(int i=0;i<(amount+1);i++){
+					getServer().dispatchCommand(sender, this.lcommand);
+				}
+				return true;
+			}
+			if(args[0].equalsIgnoreCase("remove")){
+				Block[] bl = hashToArray(clip);
+				for(Block b : bl){
+					if(clip.get(b).getDisplayName().equalsIgnoreCase(((Player)sender).getDisplayName())){
+						b.setType(Material.AIR);
+					}
+				}
+			}
+			if(args[0].equalsIgnoreCase("type")){
+				Block[] bl = hashToArray(clip);
+				if(args.length==1){
+					sender.sendMessage("Invalid amount of arguments");
+					return true;
+				}
+				if(args.length==2){
+					for(Block b : bl){
+						if(clip.get(b).getDisplayName().equalsIgnoreCase(((Player)sender).getDisplayName())){
+							Material mat = Material.valueOf(args[1]);
+							if(mat==null){
+								sender.sendMessage("No Material named "+args[1]);
+								return true;
+							}
+							b.setType(mat);
+						}
+					}
+					return true;
+				}
+				if(args.length>2){
+					for(Block b : bl){
+						if(clip.get(b).getDisplayName().equalsIgnoreCase(((Player)sender).getDisplayName())){
+							Material mat = Material.valueOf(args[1]);
+							Material mat2 = Material.valueOf(args[2]);
+							if(mat==null){
+								sender.sendMessage("No Material named "+args[1]);
+								return true;
+							}
+							if(b.getType()==mat2){
+								b.setType(mat);
+							}
+						}
+					}
+					return true;
+				}
 			}
 		}
 		return false;
 	}
-	public Material getSelectionTool(){
-		Material mat = Material.valueOf(tool_name.toUpperCase());
-		if(mat==null){
-			mat = Material.STICK;
+	public Object[] getSelectionTool(){
+		Object[] mat=new Object[2];
+		mat[0] = Material.valueOf(this.tool_name.toUpperCase());
+		if(mat[0]==null){
+			mat[0] = Material.STICK;
 		}
+		mat[1]=this.tool_id;
+		return mat;
+	}
+	
+	public Object[] getCubeSelectionTool(){
+		Object[] mat=new Object[2];
+		mat[0] = Material.valueOf(this.cube_tool_name.toUpperCase());
+		if(mat[0]==null){
+			mat[0] = Material.STICK;
+		}
+		mat[1]=this.cube_tool_id;
 		return mat;
 	}
 	
@@ -221,19 +369,64 @@ public class Mega extends JavaPlugin{
 	 * @param player : the player who clicked
 	 * @return a boolean - false if block was removed and true if it was added
 	 */
-	public boolean addClip(Block block, Player player){
+	public boolean addClip(Block block, Player player, boolean msg){
 		if(clip.containsKey(block)){
 			if(clip.get(block).getDisplayName().equalsIgnoreCase(player.getDisplayName())){
 				clip.remove(block);
-				player.sendMessage("Block Removed From Clipboard");
+				if(msg){
+					player.sendMessage("Block Removed From Clipboard");
+				}
 				return false;
 			}else{
-				player.sendMessage("Someone Else Has Selected This Block");
+				if(msg){
+					player.sendMessage("Someone Else Has Selected This Block");
+				}
 				return false;
 			}
 		}else{
 			clip.put(block, player);
+			if(msg){
+				player.sendMessage("Block Added To Clipboard");
+			}
 			return true;
 		}
+	}
+	public String assemble(String[] sl, String sep){
+		String ret="";
+		for(String s : sl){
+			ret+=s;
+			ret+=sep;
+		}
+		return ret;
+	}
+	public void registerLastCommand(String label, String[] args){
+		this.lcommand=label+" "+assemble(args, " ");
+	}
+	public void setC1(Player p, Location l){
+		this.c1.put(p, l);
+	}
+	public void setC2(Player p, Location l){
+		this.c2.put(p, l);
+	}
+	public Location getC1(Player p){
+		if(!this.c1.containsKey(p)){
+			return p.getLocation();
+		}
+		return this.c1.get(p);
+	}
+	public Location getC2(Player p){
+		if(!this.c2.containsKey(p)){
+			return p.getLocation();
+		}
+		return this.c2.get(p);
+	}
+	public Object[] getCuboidSelectTool(){
+		Object[] mat=new Object[2];
+		mat[0] = Material.valueOf(this.cube_tool_name.toUpperCase());
+		if(mat[0]==null){
+			mat[0] = Material.STRING;
+		}
+		mat[1]=this.cube_tool_id;
+		return mat;
 	}
 }
--- src/org/styllex/clipboard/listener/IC3D_Listener_10_90_13.java
+++ src/org/styllex/clipboard/listener/IC3D_Listener_10_90_13.java
@@ -1,5 +1,7 @@
 package org.styllex.clipboard.listener;
 
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.block.Action;
@@ -7,16 +9,60 @@ import org.bukkit.event.player.*;
 import org.styllex.clipboard.Mega;
 
 public class IC3D_Listener_10_90_13 extends PlayerListener{
+	
 	private Mega plugin;
+	
 	public IC3D_Listener_10_90_13(Mega instance){
 		this.plugin=instance;
 	}
+	
 	public void onPlayerInteract(PlayerInteractEvent event){
 		if(event.getAction()==Action.RIGHT_CLICK_BLOCK){
 			Player player = event.getPlayer();
 			Block block = event.getClickedBlock();
-			if(player.getItemInHand().getType()==plugin.getSelectionTool()){
-				plugin.addClip(block, player);
+			if(player.getItemInHand().getType()==((Material)plugin.getSelectionTool()[0]))
+			{
+				plugin.addClip(block, player, true);
+			}
+			if(player.getItemInHand().getTypeId()==((int)((Integer)plugin.getSelectionTool()[1])))
+			{
+				plugin.addClip(block, player, true);
+			}
+			if(player.getItemInHand().getType()==((Material)plugin.getCuboidSelectTool()[0]))
+			{
+				if(plugin.c3.containsKey(player)){
+					if(plugin.c3.get(player)==1){
+						plugin.setC1(player, event.getClickedBlock().getLocation());
+						player.sendMessage(ChatColor.GOLD+"You Selected Corner 1 Of The Cuboid!");
+						plugin.c3.put(player, 2);
+					}else{
+						plugin.setC2(player, event.getClickedBlock().getLocation());
+						player.sendMessage(ChatColor.GOLD+"You Selected Corner 2 Of The Cuboid!");
+						plugin.c3.put(player, 1);
+					}
+				}else{
+					plugin.setC1(player, event.getClickedBlock().getLocation());
+					player.sendMessage(ChatColor.GOLD+"You Selected Corner 1 Of The Cuboid!");
+					plugin.c3.put(player, 2);
+				}
+			}
+			if(player.getItemInHand().getTypeId()==((Integer)plugin.getCuboidSelectTool()[1]))
+			{
+				if(plugin.c3.containsKey(player)){
+					if(plugin.c3.get(player)==1){
+						plugin.setC1(player, event.getClickedBlock().getLocation());
+						player.sendMessage(ChatColor.GOLD+"You Selected Corner 1 Of The Cuboid!");
+						plugin.c3.put(player, 2);
+					}else{
+						plugin.setC2(player, event.getClickedBlock().getLocation());
+						player.sendMessage(ChatColor.GOLD+"You Selected Corner 2 Of The Cuboid!");
+						plugin.c3.put(player, 1);
+					}
+				}else{
+					plugin.setC1(player, event.getClickedBlock().getLocation());
+					player.sendMessage(ChatColor.GOLD+"You Selected Corner 1 Of The Cuboid!");
+					plugin.c3.put(player, 2);
+				}
 			}
 		}
 	}
